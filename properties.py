6493e719acf34e2b8fedecbe1b305783c3a483f6
Rajarshi Sarkar <rikirockers@gmail.com>
2017-09-04 20:58:13 +0530

    Second Commit -- Modularised Phase 1 - data_query modularised

--- properties.py
+++ properties.py
@@ -1,2 +1,50 @@
-appname="test-cona"
-mode="yarn-client"
+# appname="test_cona"
+# mode="yarn-client"
+
+#########################_____DATA_INGESTION_PARAMETERS_____#########################
+
+query="""select d.customernumber customernumber, d.matnr matnr, d.bill_date bill_date, IF(d.units != 'CS', d.quantity * (f.umrez / f.umren), d.quantity) quantity, ((quantity * quantity) / d.price) q_indep_prc
+        from
+        (
+        select b.customernumber customernumber, b.matnr matnr, b.bill_date bill_date ,b.quantity quantity, b.units units, b.price price
+        from
+        (
+        select a.kunag customernumber, a.matnr matnr, a.fkdat bill_date ,a.fklmg quantity, a.meins units, a.netwr price
+        from skuopt.invoices a
+        ) b
+        join
+        (
+        select kunnr customernumber
+        from mdm.customer
+        where vkbur='C001'
+        ) c
+        on
+        b.customernumber = c.customernumber
+        ) d
+        join
+        (
+        select e.matnr matnr, e.meinh meinh, e.umren umren, e.umrez umrez
+        from mdm.dim_marm e
+        ) f
+        on
+        d.matnr=f.matnr and d.units=f.meinh"""
+
+_latest_product_criteria_days=92
+_product_criteria_days=1030
+_product_criteria_years=2.0
+_product_criteria_annual_frequency=60
+
+###############################################################################
+
+#########################_____MODELING_PARAMETERS_____#########################
+
+
+
+###############################################################################
+
+
+#########################_____EXPERIMENTAL_____#########################
+# Experimental
+# include_neg_quantity=False
+
+###############################################################################
--- run.py
+++ run.py
@@ -1,6 +1,37 @@
 from pyspark import SparkConf,SparkContext
 from properties import *
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import HiveContext
+from data_query import getData
+from support_func import model_fit
 
-conf = SparkConf().setAppName(appname).setMaster(mode)
+conf = SparkConf().setAppName("test_cona").setMaster("yarn-client")
 sc = SparkContext(conf=conf)
+sqlContext = HiveContext(sparkContext=sc)
+
+print "Setting LOG LEVEL as ERROR"
+sc.setLogLevel("ERROR")
+
+print "Adding Extra paths for several site-packages"
+import sys
+sys.path.append('/home/SSHAdmin/.local/lib/python2.7/site-packages/')
+sys.path.append('/home/SSHAdmin/anaconda/lib/python2.7/site-packages/')
+# sys.path.insert(0, cona_modules.zip)
+
+print "Reading Holidays file"
+holidays_data = sc.textFile("/tmp/pyspark_data/holidays")
+header = holidays_data.first()
+holidays_rdd = holidays_data.filter(lambda x: x != header).map(lambda x: x.split(","))
+holiday_list = sqlContext.createDataFrame(holidays_rdd, ['ds', 'holiday', 'lower_window', 'upper_window']).collect()
+
+print "Querying of Hive Table - Obtaining Product Data"
+# sc.broadcast(holidays)
+test_data = getData(sqlContext=sqlContext)
+test_data_rdd = test_data.map(lambda x: model_fit(x, holiday_list))
+
+print "Output - take(1)"
+print test_data_rdd.take(1)
+
+
+
 
