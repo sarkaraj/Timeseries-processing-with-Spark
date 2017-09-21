ab837cd660dad71e0aa5f22af28ca87eeccedf95
Ravi Yadav <raviy8408@gmail.com>
2017-09-21 12:37:07 +0530

    Merge with master

--- model/run_multiple_prod.py
+++ model/run_multiple_prod.py
@@ -17,7 +17,7 @@ rcParams['figure.figsize'] = 15, 6
 file_dir = "C:\\files\\CONA_Conv_Store_Data\\"
 
 # image save folder
-image_dir = "C:\\files\\CONA_Conv_Store_Data\\temp\\weekly_ensm\\temp_ma\\"
+image_dir = "C:\\files\\CONA_Conv_Store_Data\\temp\\weekly_ensm\\temp\\"
 
 # holidays
 holidays = pd.read_table(file_dir + 'holidays.csv', delimiter=',', header=0)
@@ -27,7 +27,7 @@ holidays.upper_window = 7
 # holidays.head(6)
 
 # data transformation to weekly and monthly aggregate
-raw_data = pd.read_csv(file_dir + "25_C005_between_30_60.tsv", sep="\t", header=None,
+raw_data = pd.read_csv(file_dir + "25_C005_greater_than_60.tsv", sep="\t", header=None,
                        names=['customernumber', 'matnr', 'date', 'quantity', 'q_indep_p'])
 data_weekly = get_weekly_aggregate(inputDF=raw_data)
 data_weekly.dt_week = data_weekly.dt_week.apply(str).apply(parser.parse)
@@ -48,12 +48,12 @@ for cus_no in data_weekly.customernumber.unique():
         max(prod.dt_week.apply(str).apply(parser.parse)) - min(prod.dt_week.apply(str).apply(parser.parse))).days
         if days > 830:
             # weekly_model_image_saver(prod, cus_no, mat_no, dir_name, holidays, min_train_days=731, test_points=2)
-            # prod_output = weekly_ensm_model(prod=prod, cus_no=cus_no, mat_no=mat_no, dir_name=image_dir)
+            prod_output = weekly_ensm_model(prod=prod, cus_no=cus_no, mat_no=mat_no, dir_name=image_dir)
 
             # monthly_prophet_model(prod, cus_no, mat_no, dir_name, min_train_days=731, test_points=1)
             # prod_output = monthly_prophet_model(prod = prod , cus_no = cus_no, mat_no = mat_no, dir_name= image_dir)
-            (prod_output, pred) = moving_average_model(prod=prod, cus_no=cus_no, mat_no=mat_no, weekly_data=False,
-                                                        weekly_window=6, monthly_window=3)
+            # (prod_output, pred) = moving_average_model(prod=prod, cus_no=cus_no, mat_no=mat_no, weekly_data=False,
+            #                                             weekly_window=6, monthly_window=3)
 
             final_data_df = pd.concat([final_data_df, prod_output], axis=0)
 
