bf8982479a3e4cc375d2359a7f0110cbd91a513d
Rajarshi Sarkar <rikirockers@gmail.com>
2018-01-04 15:46:40 +0530

    data_query doc added

--- data_fetch/README.md
+++ data_fetch/README.md
@@ -40,6 +40,41 @@ Retrieve data from Hive tables.
   2. ##### properties
   
         1.``_query`` : Primary query for data extraction from Hive tables
+        
         2.``CUSTOMER_LIST`` : Optional parameter to specify a set of customers into the query. There are existing two sets of query. This works only with a specific one. Use it appropriately 
   ------
+  
+  3. ##### support_func
+        1.``_get_weekly_mdl_bld_cutoff_date`` : Input should ONLY be Sundays when model building is performed. Used in the initial data query from Hive
+        
+            :``param _date``: Date of SUNDAY in datetime.date type
+            
+            :``return``: String of Date used for query --> last Saturday to the corresponding Sunday
+            
+        ------
+    
+        2.``_get_monthly_mdl_bld_cutoff_date`` : Input should ONLY be Sundays when model building is performed. Used in the initial data query from Hive
+        
+            :``param _date``: Date of SUNDAY in datetime.date type
+            
+            :``return``: String of Date used for query --> date of the last day of previous month is string format
+        
+        ------
+    
+        3.``generate_weekly_query`` : Generate data extraction query for weekly run
+        
+            :param date: String:: Date in ('yyyy-MM-dd') format. The date when the model is executed
+            
+            :return: String:: Complete hive query
+        
+        ------
+    
+        4.``generate_monthly_query`` : Generate data extraction query for monthly run
+        
+            :param date:  String:: Date in ('yyyy-MM-dd') format. The date when the model is executed
+            
+            :return:  String:: Complete hive query
+            
+  ------
+
         
--- data_fetch/support_func.py
+++ data_fetch/support_func.py
@@ -5,7 +5,7 @@ def _get_weekly_mdl_bld_cutoff_date(_date):
     """
     Input should ONLY be Sundays when model building is performed. Used in the initial data query from Hive
     :param _date: Date of SUNDAY in datetime.date type
-    :return: String of Date used for query
+    :return: String of Date used for query --> last Saturday to the corresponding Sunday
     """
     _result = _date.strftime('\'%Y%m%d\'')
     return _result
@@ -22,6 +22,11 @@ def _get_monthly_mdl_bld_cutoff_date(_date):
 
 
 def generate_weekly_query(date):
+    """
+    Generate data extraction query for weekly run
+    :param date: String:: Date in ('yyyy-MM-dd') format. The date when the model is executed
+    :return: String:: Complete hive query
+    """
     from properties import _query
 
     MODEL_BLD_CURRENT_DATE = string_to_gregorian(date)
@@ -31,6 +36,11 @@ def generate_weekly_query(date):
 
 
 def generate_monthly_query(date):
+    """
+    Generate data extraction query for monthly run
+    :param date:  String:: Date in ('yyyy-MM-dd') format. The date when the model is executed
+    :return:  String:: Complete hive query
+    """
     from properties import _query
 
     MODEL_BLD_CURRENT_DATE = string_to_gregorian(date)
@@ -40,14 +50,22 @@ def generate_monthly_query(date):
 
 
 if __name__ == "__main__":
-    import datetime
-
-    # today = string_to_gregorian('2017-10-11')
-    # first = today.replace(day=1)
-    # print first
-    # lastMonth = first - datetime.timedelta(days=1)
-    # print lastMonth
-    # print lastMonth.strftime('\'%Y%m%d\'')
-    input = '2017-09-01'
-    print generate_monthly_query(input)
-    print generate_weekly_query(input)
+    print (_get_weekly_mdl_bld_cutoff_date.__name__)
+    print (_get_weekly_mdl_bld_cutoff_date.__doc__)
+    print (_get_monthly_mdl_bld_cutoff_date.__name__)
+    print (_get_monthly_mdl_bld_cutoff_date.__doc__)
+    print (generate_weekly_query.__name__)
+    print (generate_weekly_query.__doc__)
+    print (generate_monthly_query.__name__)
+    print (generate_monthly_query.__doc__)
+    # import datetime
+    #
+    # # today = string_to_gregorian('2017-10-11')
+    # # first = today.replace(day=1)
+    # # print first
+    # # lastMonth = first - datetime.timedelta(days=1)
+    # # print lastMonth
+    # # print lastMonth.strftime('\'%Y%m%d\'')
+    # input = '2017-09-01'
+    # print generate_monthly_query(input)
+    # print generate_weekly_query(input)
