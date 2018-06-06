a9364c87c8977ec78798fa75e33d90ace936a6eb
Ravi Yadav <raviy8408@gmail.com>
2018-06-06 14:50:05 +0530

    Changes Made:
    1) Outlier removal part taken out as it might have been taking extra time
    2) test run on sample of 5 customers
    3) location is again testing files

--- data_fetch/custom_customer_list.py
+++ data_fetch/custom_customer_list.py
@@ -109,8 +109,7 @@ _customer_list = """
 
 def generate_customer_list_fomatted(customer_list=_customer_list, string_to_append="0"):
     result_list = [["".join([string_to_append + str(elem)])] for elem in customer_list.split("\n") if len(elem) > 0]
-    # TODO
-    return result_list[:5]
+    return result_list
 
 
 if __name__ == "__main__":
