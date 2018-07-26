556976142213e7d811fc900a40e0023531526a82
Ravi Yadav <raviy8408@gmail.com>
2018-07-26 20:47:29 +0530

    Changes Made:
    1. All ts are are now weekly
    2. category definitions changed accordingly
    3. outlier removal definitions changed
    4. error calculator for ma are appended with new errors
    5. files would still save at the same earlier define locations as the category
    
    current run:
    
    running only for ma categories 4 to 10

--- _weekly_products.py
+++ _weekly_products.py
@@ -85,7 +85,7 @@ def build_prediction_weekly(sc, sqlContext, **kwargs):
 
     ma_weekly_results_df_final\
         .withColumn("category_flag", udf(lambda x: x.get("category"), StringType())(col("pdt_cat")).cast(StringType())) \
-        .filter(col('category_flag').isin(['IV', 'V', 'VI'])).show()
+        .filter(col('category_flag').isin(['IV', 'V', 'VI'])).distinct(col('category_flag')).show()
         # .coalesce(5) \
         # .write.mode(p.WRITE_MODE) \
         # .format('orc') \
