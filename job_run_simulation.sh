662953d9638dbae49440513799cd34be94941296
Rajarshi <rajarshisarkar21@gmail.com>
2018-08-26 21:10:48 +0530

    Testing simulation script after bug-fix

--- job_run_simulation.sh
+++ job_run_simulation.sh
@@ -13,25 +13,24 @@ do
   esac
 done
 
-echo $model_bld_date
 
-#spark-submit \
-#--verbose \
-#--master yarn \
-#--deploy-mode client \
-#--num-executors 35 \
-#--driver-memory 5G \
-#--queue tsmdl \
-#--executor-memory 4G \
-#--executor-cores 2 \
-#--conf spark.dynamicAllocation.enabled=false \
-#--conf spark.speculation=true \
-#--conf spark.speculation.multiplier=1.5 \
-#--conf spark.speculation.quantile=0.9 \
-#--conf spark.speculation.interval=18000 \
-#--conf spark.sql.shuffle.partitions=70 \
-#--py-files $HOME/cso_simulator_master/cso_predictor_prod/forecaster.zip \
-#$HOME/cso_simulator_master/cso_predictor_prod/run_sim.py \
-#$model_bld_date
+spark-submit \
+--verbose \
+--master yarn \
+--deploy-mode client \
+--num-executors 35 \
+--driver-memory 4G \
+--queue tsmdl \
+--executor-memory 3G \
+--executor-cores 2 \
+--conf spark.dynamicAllocation.enabled=false \
+--conf spark.speculation=true \
+--conf spark.speculation.multiplier=1.5 \
+--conf spark.speculation.quantile=0.9 \
+--conf spark.speculation.interval=18000 \
+--conf spark.sql.shuffle.partitions=70 \
+--py-files $HOME/cso_simulator_master/cso_predictor_prod/forecaster.zip \
+$HOME/cso_simulator_master/cso_predictor_prod/run_sim.py \
+$model_bld_date
 
 exit 0
