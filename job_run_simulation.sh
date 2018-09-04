2e2d1ee7364aacd68fae93877cb3480c88b43298
Rajarshi <rajarshisarkar21@gmail.com>
2018-09-04 12:02:00 +0530

    Validation test
    1. Running simulation for past 8 weeks -- Initial Commit

--- job_run_simulation.sh
+++ job_run_simulation.sh
@@ -19,18 +19,19 @@ spark-submit \
 --master yarn \
 --deploy-mode client \
 --num-executors 35 \
---driver-memory 10G \
---executor-memory 5G \
---queue tsmdl \
+--driver-memory 15G \
+--executor-memory 10G \
 --executor-cores 2 \
 --conf spark.dynamicAllocation.enabled=false \
 --conf spark.speculation=true \
 --conf spark.speculation.multiplier=1.5 \
 --conf spark.speculation.quantile=0.9 \
 --conf spark.speculation.interval=18000 \
---conf spark.sql.shuffle.partitions=70 \
+--conf spark.sql.shuffle.partitions=45 \
 --py-files $HOME/cso_simulator_master/cso_predictor_prod/forecaster.zip \
 $HOME/cso_simulator_master/cso_predictor_prod/run_sim.py \
 $model_bld_date
 
 exit 0
+
+# --queue tsmdl \
